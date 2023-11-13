6f8e4d0463c06f50f477ca7fcccf91e561d41b8f
TONGTONGDESHIJIE <1090060206@qq.com>
2023-11-13 15:50:32 +0800

    修复运行中的警告

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,6 +15,8 @@ module.exports = {
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'space-before-function-paren': 'off',
     'vue/multi-word-component-names': 'off',
-    'no-unused-vars': 'warn'
+    'no-unused-vars': 'warn',
+    'eslint-disable-next-line': 'off',
+    'no-undef': 'off'
   }
 }
