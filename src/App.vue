d758bd7f3714b4accf9dcbecefd356e3d2600085
TONGTONGDESHIJIE <1090060206@qq.com>
2023-11-14 16:31:22 +0800

    创建文章基本结构

--- src/App.vue
+++ src/App.vue
@@ -10,4 +10,32 @@ const store = useStore()
 generateNewStyle(store.getters.mainColor).then((newStyleText) => {
   writeNewStyle(newStyleText)
 })
+
+const debounce = (fn, delay) => {
+  let timer = null
+
+  return function () {
+    const context = this
+
+    const args = arguments
+
+    clearTimeout(timer)
+
+    timer = setTimeout(function () {
+      fn.apply(context, args)
+    }, delay)
+  }
+}
+
+// 解决ERROR ResizeObserver loop completed with undelivered notifications.
+
+const _ResizeObserver = window.ResizeObserver
+
+window.ResizeObserver = class ResizeObserver extends _ResizeObserver {
+  constructor(callback) {
+    callback = debounce(callback, 16)
+
+    super(callback)
+  }
+}
 </script>
