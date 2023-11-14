e4724b0a304e158b2800a60158e6cc18459d6a05
TONGTONGDESHIJIE <1090060206@qq.com>
2023-11-14 14:53:44 +0800

    作者模块开发

--- src/views/profile/components/Author.vue
+++ src/views/profile/components/Author.vue
@@ -1,3 +1,41 @@
 <template>
-  <div>Author</div>
+  <div class="author-container">
+    <div class="header">
+      <my-avatar image="https://i2.100024.xyz/2023/01/26/3e727b.webp" height="60px" width="60px"> </my-avatar>
+      <div class="header-desc">
+        <h3>陈嘉淇</h3>
+        <span>一名软件工程专业的学生</span>
+      </div>
+    </div>
+    <div class="info">Web 后端开发工程师</div>
+  </div>
 </template>
+
+<script setup>
+import MyAvatar from '@/components/MyAvatar/index.vue'
+import {} from 'vue'
+</script>
+
+<style lang="scss" scoped>
+.author-container {
+  .header {
+    display: flex;
+    .header-desc {
+      margin-left: 12px;
+      display: flex;
+      flex-direction: column;
+      justify-content: space-around;
+
+      span {
+        font-size: 14px;
+      }
+    }
+  }
+  .info {
+    margin-top: 16px;
+    line-height: 22px;
+    font-size: 14px;
+    text-indent: 26px;
+  }
+}
+</style>
